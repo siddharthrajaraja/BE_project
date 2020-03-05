a55c7e39c3ad779f88305bc18377a18df56d5038
Amankumar Shrivastava <shrivastavaman171@gmail.com>
2020-03-05 10:08:27 +0530

    Successive fractional differentiation

---
+++ Library/Fd_Successive_Differentiation.py
@@ -0,0 +1,20 @@
+import math
+import matplotlib.pyplot as plt 
+def successive_differentiation(mu,alpha):
+    i=0
+    f=[]
+    t=[]
+    dio=[]
+    dfo=[]
+    while(i<10):
+        t.append(i)
+        f.append(pow(t[-1],mu))
+        dio.append(mu*pow(t[-1],mu-1))
+        dfo.append((math.gamma(mu+1)/math.gamma(mu-alpha+1))*pow(t[-1],(mu-alpha)))
+        i+=0.01
+
+    print(len(t),len(f),len(dio),len(dfo))
+    plt.plot(t,f,color="blue")
+    plt.plot(t,dio,color="red")
+    plt.plot(t,dfo,color="green")
+successive_differentiation(1.2,0.1)
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Fractional-Calculus
 Final Year Project
+
+Fd_Successive_Differentiation.py:
+    It includes a function which returns the fractional calculus(alpha) for the polynomial of the form (t^mu) and plots the graph for the same.
