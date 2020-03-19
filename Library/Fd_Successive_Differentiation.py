bdbc0396463718d0193186c569347a85eef8d51a
SIDDHARTH RAMESH CHAND RAJA <45534008+siddharthrajaraja@users.noreply.github.com>
2020-03-19 10:33:34 +0530

    removed matplotlb and import plotly lib

--- Library/Fd_Successive_Differentiation.py
+++ Library/Fd_Successive_Differentiation.py
@@ -1,9 +1,13 @@
 import math
-import matplotlib.pyplot as plt 
+import numpy as np
+import plotly.graph_objs as go
+import plotly.offline as ply
+
+
 def successive_differentiation(mu,alpha):
     i=0
-    f=[]
-    t=[]
+    f=[]   # x
+    t=[]   # x axis
     dio=[]
     dfo=[]
     while(i<10):
@@ -13,9 +17,48 @@ def successive_differentiation(mu,alpha):
         dfo.append((math.gamma(mu+1)/math.gamma(mu-alpha+1))*pow(t[-1],(mu-alpha)))
         i+=0.01
 
+    
     print(len(t),len(f),len(dio),len(dfo))
-    plt.plot(t,f,color="blue")
-    plt.plot(t,dio,color="red")
-    plt.plot(t,dfo,color="green")
-    plt.show()
+    trace1=go.Scatter(x=t,y=f,line={'color':'blue'},name="t-f")
+    trace2=go.Scatter(x=t,y=dio,line={'color':'red'},name="t-dio")
+    trace3=go.Scatter(x=t,y=dfo,line={'color':'green'},name="t-dfo")
+    
+    layout = go.Layout(
+    title=go.layout.Title(
+        text='Fractional Derivative',
+        xref='paper',
+        x=0
+    ),
+    xaxis=go.layout.XAxis(
+        title=go.layout.xaxis.Title(
+            text='t',
+            font=dict(
+                family='Courier New, monospace',
+                size=18,
+                color='#7f7f7f'
+            )
+        )
+    ),
+    yaxis=go.layout.YAxis(
+        title=go.layout.yaxis.Title(
+            text='f / dio / dfo',
+            font=dict(
+                family='Courier New, monospace',
+                size=18,
+                color='#7f7f7f'
+                )
+            )
+        )
+    )
+
+    data=[trace1,trace2,trace3]
+    fig=go.Figure(data,layout)
+    ply.plot(fig,filename="../web_pages/render.html")
+
+    
+
+    # plt.plot(t,f,color="blue")
+    # plt.plot(t,dio,color="red")
+    # plt.plot(t,dfo,color="green")
+    # plt.show()
 successive_differentiation(1.2,0.1)
